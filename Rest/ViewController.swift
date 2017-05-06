8f7ca85d5be2a379bdb4cffdffbdb959cf721a02
Arthur Papailhau <papailhau@gmail.com>
2017-05-06 14:29:46 +0200

    Initial Commit

---
+++ Rest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rest
+//
+//  Created by Arthur Papailhau on 5/6/17.
+//  Copyright © 2017 Arthur Papailhau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
