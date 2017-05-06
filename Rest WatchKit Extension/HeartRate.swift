634c9d13559a109f4be764f9b5af2410cc25b967
Arthur Papailhau <papailhau@gmail.com>
2017-05-06 16:09:01 +0200

    feat(Authorization): Add UI, ask authorization, display heart rate information

---
+++ Rest WatchKit Extension/HeartRate.swift
@@ -0,0 +1,16 @@
+//
+//  HeartRate.swift
+//  Rest
+//
+//  Created by Arthur Papailhau on 5/6/17.
+//  Copyright © 2017 Arthur Papailhau. All rights reserved.
+//
+
+import Foundation
+
+struct HeartRate {
+
+    let timestamp: Date
+    let bpm: Double
+
+}
--- Rest WatchKit Extension/InterfaceController.swift
+++ Rest WatchKit Extension/InterfaceController.swift
@@ -11,20 +11,59 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
+    @IBOutlet var heartRateLabel: WKInterfaceLabel!
+    @IBOutlet var controlButton: WKInterfaceButton!
+
+    private let restSessionManager = RestSessionManager()
+
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
 
         // Configure interface objects here.
     }
 
+    private let heartRateManager = HeartRateManager()
+
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        restSessionManager.delegate = self
+    }
+
+    // MARK: - Actions
+
+    @IBAction func didTapButton() {
+        switch restSessionManager.state {
+        case .started:
+            // Stop current workout.
+            restSessionManager.stop()
+            break
+        case .stopped:
+            // Start new workout.
+            restSessionManager.start()
+            break
+        }
     }
 
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
+    }
+
+}
+
+// MARK: - Rest Session Manager Delegate
+
+extension InterfaceController: RestSessionManagerDelegate {
+
+    func restSessionManager(_ manager: RestSessionManager, didChangeStateTo newState: RestSessionState) {
+        // Update title of control button.
+        controlButton.setTitle(newState.actionText())
+    }
+
+    func restSessionManager(_ manager: RestSessionManager, didChangeHeartRateTo newHeartRate: HeartRate) {
+        // Update heart rate label.
+        heartRateLabel.setText(String(format: "%.0f", newHeartRate.bpm))
     }
 
 }
